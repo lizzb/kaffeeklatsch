218a08de313b7ddf2b78359eea8b1b5b6077ea3a
Kaustubh Garimella <kaustubh.garimella@gmail.com>
2013-06-06 19:52:47 -0500

    Comments, game constant

--- Assets/Scripts/EndGame.cs
+++ Assets/Scripts/EndGame.cs
@@ -20,32 +20,51 @@ public class EndGame : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		if(cafe.moneyManager.funds <= 0){
-			isBankrupt = true;
+		if(cafe.moneyManager.funds <= 0){ //If funds falls below 0
+			isBankrupt = true; //Set bankrupt to true
 		}
-		if(clock.days == 6){
-			if(cafe.moneyManager.funds >= GameConstants.moneyGoalWin){
-				hasWon = true;
+		if(clock.days == GameConstants.maxNumberOfDays){
+			if(cafe.moneyManager.funds >= GameConstants.moneyGoalWin){ //If funds exceeds goal
+				hasWon = true; //Set win to true
 			} else{
-				hasLost = true;
+				hasLost = true; //Otherwise set lost to true
 			}
 		}
 	}
 	
 	void OnGUI(){
-		bankrupt(isBankrupt);
+		//Call functions based on end game condition
+		bankrupt(isBankrupt); 
 		timeWin (hasWon);
 		timeLose (hasLost);
 	}
 	
+/*---------------------------------------------------------------------------
+  Name   :  bankrupt
+  Purpose:  shows end of game if user goes bankrupt
+  Receive:  value whether user has gone bankrupt
+  Return :  nothing, just ui
+---------------------------------------------------------------------------*/
 	public void bankrupt(bool bankrupt){
 		
 	}
 	
+/*---------------------------------------------------------------------------
+  Name   :  timeWin
+  Purpose:  shows end of game if user has gotten to goal in time allotted
+  Receive:  bool whether player has won
+  Return :  nothing, just ui
+---------------------------------------------------------------------------*/
 	public void timeWin(bool win){
 		
 	}
 	
+/*---------------------------------------------------------------------------
+  Name   :  timeLose
+  Purpose:  shows end of game if user hasn't gotten to goal in time allotted
+  Receive:  bool whether player has lost
+  Return :  nothing, just ui
+---------------------------------------------------------------------------*/
 	public void timeLose(bool lose){
 		
 	}
